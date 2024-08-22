0f53b6d095755605a5e0dbd173e86306916e2511
tgkim <tgkim@tiumsolutions.com>
2024-08-22 17:03:43 +0900

    initial main branch

---
+++ tgkim_visionOS_VR/tgkim_visionOS_VRApp.swift
@@ -0,0 +1,17 @@
+//
+//  tgkim_visionOS_VRApp.swift
+//  tgkim_visionOS_VR
+//
+//  Created by Tium Solutions on 8/22/24.
+//
+
+import SwiftUI
+
+@main
+struct tgkim_visionOS_VRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
